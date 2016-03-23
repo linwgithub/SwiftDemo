25559453661d21580add17c3d99e66f7f805e57d
linwcoder <770601773@q.com>
2016-03-23 13:43:55 +0800

    提交项目

---
+++ SwiftDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDemo
+//
+//  Created by pollysoft on 16/3/23.
+//  Copyright (c) 2016年 microi. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
