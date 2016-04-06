48ec03ca7011919c0efa544531dc079f210145de
linwcoder <770601773@q.com>
2016-04-06 17:39:06 +0800

    new func

---
+++ SwiftDemo/StringLearn.swift
@@ -0,0 +1,17 @@
+//
+//  StringLearn.swift
+//  SwiftDemo
+//
+//  Created by pollysoft on 16/4/1.
+//  Copyright © 2016年 microi. All rights reserved.
+//
+
+
+import Foundation
+
+class StringLearn: NSObject {
+
+    
+//    func StringUse(){
+//    }
+}
--- SwiftDemo/main.swift
+++ SwiftDemo/main.swift
@@ -22,3 +22,74 @@ let partA = "hello"
 let partB = 14
 let total = partA + String(partB)
 print(total)
+
+var mystr = "test"
+
+if mystr.isEmpty {
+    print("mystr is empty")
+}else{
+    print("mystris not empty:"+mystr)
+}
+
+//for character in "dogsddss??" {
+//    print(character)
+//}
+let forinStr = "tee"
+for c in forinStr.characters{
+    print(c)
+}
+
+var variableString = "Horse"
+var iableString = "super"
+let css = variableString + iableString
+// variableString 现在为 "Horse and carriage"
+print(css)
+
+//比较相同
+let quotation = "We're a lot alike, you and I."
+let sameQuotation = "We're a lot alike, you and I."
+if quotation == sameQuotation {
+    print("These two strings are considered equal")
+}
+
+//比较前缀
+let romeoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
+
+var act1SceneCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasPrefix("Act 1 ") {
+        ++act1SceneCount
+    }
+}
+print("There are \(act1SceneCount) scenes in Act 1")
+// prints "There are 5 scenes in Act 1"
+
+
+var mansionCount = 0
+var cellCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasSuffix("Capulet's mansion") {
+        ++mansionCount
+    } else if scene.hasSuffix("Friar Lawrence's cell") {
+        ++cellCount
+    }
+}
+print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
+// prints "6 mansion scenes; 2 cell scenes"
+
+let bigsmil = "bbbsssmmm"
+
+print(bigsmil.uppercaseString)
+print(bigsmil.lowercaseString)
