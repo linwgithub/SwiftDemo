375ce224d248199696ae5153f34bf79598de7288
linwCoder <770601773@qq.com>
2016-06-19 00:25:02 +0800

    流程控制

--- SwiftDemo/main.swift
+++ SwiftDemo/main.swift
@@ -114,3 +114,70 @@ print(gettingName)
 //2 提供默认值
 let newName = optionalName ?? "new User"
 print(newName)
+
+//
+//  main.swift
+//  SwiftDemo
+//
+//  Created by pollysoft on 16/3/23.
+//  Copyright (c) 2016年 microi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
+
+var totals = 0
+for i in 0 ..< 4 {
+    totals += i
+}
+print("\(totals)")
+
+func subNumber(sorces : [Int]) -> (max : Int, min : Int, sum : Int){
+    var max = sorces[0]
+    var min = sorces[0]
+    var sum = 0
+    for sorce in sorces{
+        if sorce > max {
+            max = sorce
+        } else if sorce < min{
+            min = sorce
+        }
+        sum += sorce
+    }
+    return (max, min, sum)
+}
+
+let numberSubResult = subNumber([1,2,3,4,5,6,7])
+
+print("\(numberSubResult.sum)")
+print("\(numberSubResult.max)")
+print("\(numberSubResult.min)")
+
