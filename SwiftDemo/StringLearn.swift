6b51666e7eb84764ff72a264746ce2546545fe1d
linwCoder <770601773@qq.com>
2016-06-22 19:59:59 +0800

    test

--- SwiftDemo/StringLearn.swift
+++ SwiftDemo/StringLearn.swift
@@ -6,13 +6,26 @@
 //  Copyright © 2016年 microi. All rights reserved.
 //
 
-
 import Foundation
 
-class StringLearn: NSObject {
-
+class StringLearn{
+    
     var stringOfStringLearn = "simple"
     func StringLearnDescript() -> String{
         return "string of StringLearn : " + stringOfStringLearn
     }
+    
+    //构造函数
+    init() {
+        self.name = "without name"//self被用来区别实例变量
+        self.stringOfStringLearn = "init Simple"
+    }
+    
+    var name : String
+    //构造函数
+    init(name : String) {
+        self.name = name
+        self.stringOfStringLearn = "init Simple"
+    }
+    
 }
